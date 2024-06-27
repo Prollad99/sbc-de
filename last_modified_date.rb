a4ed858c575d3dc5702ac833ae420c02506d7dc4
Prollad Mondal <prolladmail@gmail.com>
2024-06-27 20:07:06 +0530

    Update last_modified_date.rb

--- last_modified_date.rb
+++ last_modified_date.rb
@@ -7,6 +7,7 @@ module Jekyll
       if index_page
         index_path = File.join(site.source, index_page.path)
         last_modified_time = File.mtime(index_path)
+        puts "Last modified time of index.html: #{last_modified_time}"  # Check if this outputs correctly
         index_page.data['last_modified_date'] = last_modified_time
       end
     end
